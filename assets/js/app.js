86ebc0a1d958f00459b9ce55046634ddfb2ae6dc
Jarret Gabel <jarretgabel@gmail.com>
2023-05-16 11:45:44 -0600

    hugo-starter

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ gsap.registerPlugin(ScrollToPlugin)
 
 import Polyfills from "./common/Polyfills"
 import Intro from './components/Intro'
+import Utils from './common/Utils'
 
 window.ESPN = window.ESPN || {}
 
@@ -14,6 +15,8 @@ class App {
 	}
 
   kickoff() {
+    Utils.replaceLogos()
+
     new Intro()
   }
 }
